87d968ca2d40c40bb23cf07c25d4f30a8987a971
navikit <navichio@gmail.com>
2016-11-14 22:43:08 -0500

    Update compressT_LOLS.c

--- compressT_LOLS.c
+++ compressT_LOLS.c
@@ -3,15 +3,42 @@
 #include <string.h>
 #include <pthread.h>
 
-void main(int argc, char** argv){
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void main(int argc, char** argv){  
+  int n = argv[2]
+  pthread_t tid[n];
+  for ( i = 0; i < n; i++){
+    pthread_create(&tid[i], NULL, compressT, argements);
+  }
+  for ( i = 0; i < n; i++){
+    pthread_join(tid[n], NULL);
+  }
   
-  pthread_t tid;
-  pthread_create(&tid, NULL, compression, NULL);
-  pthread_join(tid, NULL);
-  exit(0);
-
+  reuturn;
 }
 
-void *compressT (char *fp , int num_of_parts){
-  fp = fopen(
-}
+FILE *make_new_file (int file_counter){
+  char file outputname[15];
+  sprintf(fileoutputname, "file....%d, file counter);
+  return fopen(fileoutputname, "w");
+          }
+
+void *compressT (FILE *fp , int num_of_parts){
+  FILE *ptr_read;
+  FILE *ptr_write;
+  char line [128];
+  
+  ptr_read = fopen(fp, "r");
+  ptr_write = make_new_file(part_number);
+  
+  while( fgets(line, sizeof(line), ptr_read) != NULL){
+      if(linecounter = 5){
+        fclose(ptr_write);
+        linecounter = 1; filecounter ++; 
+        if (! ptr_writefile){
+          return 1;
+        }
+      }
+  }
+} 
