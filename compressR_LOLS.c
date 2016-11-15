f3ce22bde60e1acd7a82c861adc8bc8380a4ea1e
navikit <navichio@gmail.com>
2016-11-14 20:41:36 -0500

    Update compressR_LOLS.c

--- compressR_LOLS.c
+++ compressR_LOLS.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char **argv){
-    if(argv[1] < 2){
+    //checks if there is a valid number of arguements
+    if(argc != 2){
       printf("Invalid number of arguements");
       return 0;
     }
+    //Creates Parameters for the file itself
+    FILE *fd 
+    fd = fopen( argv[1], "r");
     
-    FILE *fp = 
+    //File doesn't exist
+    if( fd == NULL ){
+      printf("This file doesn't exist");
+      return 0;
+    }
+    
