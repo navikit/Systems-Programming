e3ea94731573af9205475be38b7ffd2fa0e75be0
navikit <navichio@gmail.com>
2016-11-15 22:32:59 -0500

    Update compressT_LOLS.c

--- compressT_LOLS.c
+++ compressT_LOLS.c
@@ -5,35 +5,48 @@
 
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
-void main(int argc, char** argv){  
-  int n = argv[1]
-  pthread_t tid[n];
-  for ( i = 0; i < n; i++){
-    pthread_create(&tid[i], NULL, compressT, arguements);
-  }
-  for ( i = 0; i < n; i++){
-    pthread_join(tid[n], NULL);
-  }
+void main(int argc, char**argv){  
+  int n = (int) argv[1];
   
-  reuturn;
+  if( n == 1){
+    pthread_t tid;
+    pthread_create(&tid, NULL, compressT, arguements);
+  }else{
+    pthread_t tid[n];
+    for ( i = 0; i < n; i++){
+      pthread_create(&tid[i], NULL, compressT, arguements);
+    }
+    for ( i = 0; i < n; i++){
+      pthread_join(tid[n], NULL);
+    }
+  }
+  return;
 }
 
-FILE *make_new_file (int file_counter){
-  char file outputname[15];
-  sprintf(fileoutputname, "file....%d, file counter);
+//makes the new file in regards to the file number
+FILE * make_new_file (int file_number){
+  char file_new_name[40];
+  sprintf(file_new_name, "%s_txt_LOLS%d, file_name, file_number);
   return fopen(fileoutputname, "w");
-          }
+}
 
-void *compressT (FILE *fp , int num_of_parts){
+void *compressT (char * argv , int partnum){
   FILE *ptr_read;
   FILE *ptr_write;
-  char line [128];
   
+  //ready file to be read, and ready new file to write
   ptr_read = fopen(fp, "r");
   ptr_write = make_new_file(part_number);
   
-  while( fgets(line, sizeof(line), ptr_read) != NULL){
-      if(linecounter = 5){
+  //start at this location
+  fseek ( ptr_read, partnum*char_per_parts, fp );
+  
+  //Dynamically allocate the desired part
+  char * line;
+  line = malloc( sizeof(char) * char_per_parts); 
+    
+  while( fgets(line, char_per_parts, ptr_read) != NULL){
+        
         fclose(ptr_write);
         linecounter = 1; filecounter ++; 
         if (! ptr_writefile){
@@ -41,4 +54,5 @@ void *compressT (FILE *fp , int num_of_parts){
         }
       }
   }
+  free (line);
 } 
